9e0058f9cc0e241bf6e72c162247e275f3b44b22
smurilogs <smurilogs@gmail.com>
2023-02-26 12:27:01 -0300

    payload formatter included

--- sensorar-enddevice/payload-formatters/lmic-node-uplink-formatters.js
+++ sensorar-enddevice/payload-formatters/lmic-node-uplink-formatters.js
@@ -1,21 +1,20 @@
-/*******************************************************************************
- *
- *  File:         lmic-node-uplink-formatters.js
- * 
- *  Function:     LMIC-node uplink payload formatter JavaScript function(s).
- * 
- *  Author:       Leonel Lopes Parente
- * 
- *  Description:  These function(s) are for use with The Things Network V3. 
- *                 
- ******************************************************************************/
 
 function decodeUplink(input) {
+    
     var data = {};
     var warnings = [];
 
     if (input.fPort == 10) {
-        data.counter = (input.bytes[0] << 8) + input.bytes[1];
+        data.temp = ((input.bytes[0] << 8) + input.bytes[1]) 
+            + (((input.bytes[2] << 8) + input.bytes[3])/100.0);
+        
+        data.rh = ((input.bytes[4] << 8) 
+        + input.bytes[5]) + (((input.bytes[6] << 8)
+            + input.bytes[7])/100.0);
+        
+        data.pm1_0 = ((input.bytes[8] << 8) | input.bytes[9]);
+        data.pm2_5 = ((input.bytes[10] << 8) | input.bytes[11]);
+        data.pm10_0 = ((input.bytes[12] << 8) | input.bytes[13]);
     }
     else {
         warnings.push("Unsupported fPort");
@@ -25,3 +24,4 @@ function decodeUplink(input) {
         warnings: warnings
     };
 }
+    
